f4af7a298ac04dbb760bf5a1fa44885e82f8ade9
Justin Wolfe <justin.wolfe@gmail.com>
2023-04-25 10:32:22 -0700

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,32 @@
+import HelpScout from "@helpscout/javascript-sdk";
+import { Button, DefaultStyle, Heading } from "@helpscout/ui-kit";
+import { useEffect, useState } from "react";
+
+function App() {
+  const [userEmail, setUserEmail] = useState<string | undefined>(
+    "unknown user"
+  );
+
+  useEffect(() => {
+    HelpScout.getApplicationContext().then(({ user }) =>
+      setUserEmail(user?.email)
+    );
+  }, []);
+
+  function onClick() {
+    HelpScout.showNoty("success", "Hello from the sidebar app");
+  }
+
+  return (
+    <div className="App">
+      <DefaultStyle />
+      <Heading level="h4">Hi, {userEmail}</Heading>
+      <br />
+      <Button size="sm" onClick={onClick}>
+        Click me
+      </Button>
+    </div>
+  );
+}
+
+export default App;
